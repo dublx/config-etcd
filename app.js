7d9f7d6488b84758a2c477fdadc0a5a6e00a49bf
Dublx <luis.faustino@gmail.com>
2014-08-19 02:19:30 +0100

    read multiple keys

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var etcd = require('nodejs-etcd');
+var etcdKeys = require('./etcd.json');
 
 var e = new etcd({
-    url: 'https://127.0.0.1:4001'
+    url: 'http://127.0.0.1:4001'
 })
+//
+//cb = e.generator(
+//    function () { console.log('An error has occurred')},
+//    function (result) { console.log(result.value)}
+//)
+//e.read(
+//    {key: '/hello'},
+//    cb
+//)
 
-cb = e.generator(
-    function () { console.log('An error has occurred')},
-    function (result) { console.log('We found the key, it has value ' + result.value)}
-)
-e.read(
-    {key: '/hello'},
-    cb
-)
+etcdKeys.keys.forEach(function (item){
+    console.log(item);
+    e.read({'key': item.key}, function (err, result, body) {
+        if (err) throw err;
+        body = JSON.parse(result.body);
+        console.log(body.node.value);
+    });
+});
+

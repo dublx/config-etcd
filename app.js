bc10eaaeb54a480eb9de783b7f7b9b276aa7cd05
Dublx <luis.faustino@gmail.com>
2014-08-19 11:21:36 +0100

    working sample

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 var etcd = require('nodejs-etcd');
-var etcdKeys = require('./etcd.json');
-
+var etcdKeys = require('./etcdKeys.json');
+var each = require('each');
 var e = new etcd({
     url: 'http://127.0.0.1:4001'
 })
-//
-//cb = e.generator(
-//    function () { console.log('An error has occurred')},
-//    function (result) { console.log(result.value)}
-//)
-//e.read(
-//    {key: '/hello'},
-//    cb
-//)
+var config = {}
 
-etcdKeys.keys.forEach(function (item){
-    console.log(item);
-    e.read({'key': item.key}, function (err, result, body) {
+each(etcdKeys.keys)
+    .parallel( true )
+    .on('item', function(element, index, next) {
+        getEtcdKeyValue(element, next);
+    })
+    .on('end', function(){
+        console.log(JSON.stringify(config, null, 2));
+    });
+
+function setConfig(name,value){
+    if (!config[name]) config[name] = [];
+    config[name].push(value);
+}
+
+function getEtcdKeyValue(item, cb){
+    e.read({'key': item.key, 'recursive':true}, function (err, result, body) {
         if (err) throw err;
         body = JSON.parse(result.body);
-        console.log(body.node.value);
+        if(!body.errorCode && !body.node.dir){
+            setConfig(item.name, body.node.value);
+        }else{
+            if (body.node.dir){
+                for (var i=body.node.nodes.length; i--; ) {
+                    setConfig(item.name, body.node.nodes[i].value);
+                }
+            }else{
+                setConfig(item.name, { "err":{"code":body.errorCode, "message": body.message , "cause": body.cause}});
+            }
+        }
+        cb();
     });
-});
+}
 
